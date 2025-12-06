8f38e71e02ad787655b41b9d4dc9bf8bc06f2e21
NeonAnurag <justasneon@gmail.com>
2025-12-06 21:45:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # ------------------------------------------------
-# File Name: config.py
+# File Name: Config.py
 # GitHub: https://github.com/MyselfNeon/
 # Telegram: https://t.me/MyelfNeon
 # Last Modified: 2025-12-06 (Multi-Owner Support)
@@ -16,10 +16,10 @@ API_HASH = os.getenv("API_HASH", "")
 BOT_TOKEN = os.getenv("BOT_TOKEN", "")
 
 # --- Authorization Config ---
-# Parse Comma-Separated IDs (e.g. "12345, 67890")
+# Comma-Separated IDs (e.g. "12345, 67890")
 raw_owner_ids = os.getenv("OWNER_ID", "841851780,7430064956")
 try:
-    # Split by comma, strip spaces, and ensure they are numbers
+    # Split by comma and ensure they are numbers
     OWNER_IDS = {int(x) for x in raw_owner_ids.split(",") if x.strip().isdigit()}
 except Exception as e:
     print(f"Error parsing OWNER_ID: {e}")
