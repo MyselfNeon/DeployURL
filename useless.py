2461c057cd91b65bc6debd962f7f44f590fda343
NeonAnurag <justasneon@gmail.com>
2025-12-29 10:47:25 +0530

    Update useless.py

--- useless.py
+++ useless.py
@@ -1,40 +1,46 @@
-# ------------------------------------------------
-# File Name: Set_Commands.py
-# Description: Auto Add Commands on new Deploys
-# ------------------------------------------------
+# ---------------------------------------------------
+# File Name: Strings.py
+# Author: NeonAnurag
+# ---------------------------------------------------
 
-import asyncio
-from pyrogram import Client
-from pyrogram.types import BotCommand
+HELP_TXT = """<b>=====  🆘 𝐇𝐄𝐋𝐏 𝐌𝐄𝐍𝐔 🆘  =====</b>
 
-# --- Edit This List ---
-SET_COMMANDS = [
-    ("start", "Check Alive Status"),
-    ("alive", "Check Bot Latency"),
-    ("help", "Get Usage Help")
-]
+<blockquote><b>‣ <i>For Private Chats</i></b></blockquote>
+<i>First Send The Invite Link Of The Chat</i>  
+<i>(Unnecessary If The String Session Account Is Already A Member).</i>  
+<i>Then Send The Post(S) Link.</i>  
 
-# --- Internal Auto Sync Logic---
-async def sync_bot_commands(app: Client):
-    # 01. Wait for the Bot to Fully connect to Telegram Servers
-    while not app.is_connected:
-        await asyncio.sleep(1)
+<blockquote><b>‣ <i>For Bot Chats</i></b></blockquote>
+<i>Send The Link With /b/, The Bot's Username, And The Message ID.</i>  
+<i>(You May Need An Unofficial Client To Get The Message ID).</i>  
 
-    print("Checking Command Sync...")
+<b><i>Example:</i></b>  
+<i>https://t.me/b/botusername/4321</i>  
 
-    try:
-        # 02. --- Format the Commands ---
-        commands = [BotCommand(cmd, desc) for cmd, desc in SET_COMMANDS]
+<blockquote><b><i>‣ Multi Posts</i></b></blockquote>
+<i>Send Public/Private Post Links As Explained Above. For Multiple Post(s)</i>  
+<i>Use The Format [Start - End] to Send Multiple Messages.</i>  
 
-        # 03. --- Push to Telegram ---
-        await app.set_bot_commands(commands)
-        
-        print(f"✓ Commands Synced with Telegram: {SET_COMMANDS}")
-    except Exception as e:
-        print(f"✗ Failed to Sync Commands: {e}")
+<b><i>Examples:</i></b>  
+<i>https://t.me/xxxx/1001-1010</i>  
+<i>https://t.me/c/xxxx/101-120</i>  
 
-# --- Plugin Entry ---
-def init(app: Client):
-    # Runs Parallel to your Bot Starting Up
-    app.loop.create_task(sync_bot_commands(app))
+<b>⚠️ <i>Spaces In Between Don’t Matter.</i></b>
+"""
 
+# --- Set Commands ---
+COMMANDS_TEXT = """
+start - 🚀 𝘊𝘩𝘦𝘤𝘬 𝘈𝘭𝘪𝘷𝘦 𝘚𝘵𝘢𝘵𝘶𝘴
+verify - 🎲 𝘎𝘦𝘵 4 𝘏𝘰𝘶𝘳𝘴 𝘍𝘳𝘦𝘦 𝘈𝘤𝘤𝘦𝘴𝘴
+help - ⁉️ 𝘏𝘰𝘸 𝘵𝘰 𝘜𝘴𝘦 𝘔𝘦
+login - 🔑 𝘓𝘰𝘨𝘪𝘯 𝘠𝘰𝘶𝘳 𝘛𝘦𝘭𝘦𝘨𝘳𝘢𝘮 𝘚𝘦𝘴𝘴𝘪𝘰𝘯
+logout - 🚪 𝘓𝘰𝘨𝘰𝘶𝘵 𝘠𝘰𝘶𝘳 𝘚𝘦𝘴𝘴𝘪𝘰𝘯
+cancel - ❌ 𝘊𝘢𝘯𝘤𝘦𝘭 𝘢𝘯𝘺 𝘖𝘯𝘨𝘰𝘪𝘯𝘨 𝘛𝘢𝘴𝘬
+users - 👥 𝘊𝘩𝘦𝘤𝘬 𝘛𝘰𝘵𝘢𝘭 𝘜𝘴𝘦𝘳𝘴 (𝘈𝘥𝘮𝘪𝘯)
+broadcast - 📢 𝘉𝘳𝘰𝘢𝘥𝘤𝘢𝘴𝘵 𝘔𝘴𝘨𝘴 𝘵𝘰 𝘜𝘴𝘦𝘳𝘴 (𝘈𝘥𝘮𝘪𝘯)
+restart - 🔄 𝘙𝘦𝘴𝘵𝘢𝘳𝘵 𝘉𝘰𝘵 𝘚𝘦𝘳𝘷𝘦𝘳𝘴 (𝘈𝘥𝘮𝘪𝘯)
+"""
+
+# Dont remove Credits
+# Developer Telegram @MyselfNeon
+# Update channel - @NeonFiles
