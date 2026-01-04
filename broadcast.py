b37fecd368d06011d5c723e82975a03aee8fe539
NeonAnurag <justasneon@gmail.com>
2026-01-04 10:55:23 +0530

    Update broadcast.py

--- broadcast.py
+++ broadcast.py
@@ -1,14 +1,14 @@
 terms_text = (
         "> 📜 **Terms & Conditions**\n\n"
-        "> **__User Responsibility__**\n"
+        "> **__01. User Responsibility__**\n"
         "• __Users Are Solely Responsible For Their Actions And Content. The Service Does Not Promote Or Support Copyrighted Or Illegal Activity.__\n\n"
-        "> **__Service Availability__**\n"
+        "> **__02. Service Availability__**\n"
         "• __Purchase Of Any Plan Does Not Guarantee Service Availability, Uptime, Or Continuity. The Service May Be Modified Or Discontinued At Any Time Without Notice.__\n\n"
-        "> **__Authorization & Access Control__**\n"
+        "> **__03. Authorization & Access Control__**\n"
         "• __User Access, Authorization, Or Banning Is Entirely At The Service Provider’s Discretion.__\n\n"
-        "> **__Payments & Feature Access__**\n"
+        "> **__04. Payments & Feature Access__**\n"
         "• __Payment Does Not Guarantee Access To Any Feature, Including The /Batch Command.__\n\n"
-        "> **__No Right of Claim__**\n"
+        "> **__05. No Right of Claim__**\n"
         "• __No Refunds, Compensation, Or Claims May Be Made For Denied Access, Service Interruptions, Or Account Restrictions.__\n\n"
 )
 
